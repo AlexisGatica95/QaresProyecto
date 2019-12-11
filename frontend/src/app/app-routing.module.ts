10c52617be5c936cf80b8ce2a4c3de322e5e6706
Juan Artola <alexisgatica95@gmail.com>
2019-12-11 18:17:53 -0300

    empece a modificar para subir una imagen

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UploadComponent } from './components/upload/upload.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+ {path: 'upload',component : UploadComponent} 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
